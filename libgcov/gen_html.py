c1caaed23dd7c3fd82b725f868c94af597bd40f7
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-02-16 11:57:48 +0100

    Adds coverage html generator and fixes gcda writer
    - using lcov or gcovr is quite tricky in this context, making a script
    that handles everything for us makes sense
    - since FSW is built from CI we either have to export all work folder or
    cheat a little and build locally the exact same code and overwrite gcda
    stamps to match gcno ones (both have different endianness)
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

---
+++ libgcov/gen_html.py
@@ -0,0 +1,44 @@
+#!/bin/env python3
+
+import os
+import argparse
+from glob import glob, escape
+import re
+import platform
+import subprocess
+
+list_files = subprocess.run(["ls", "-l"])
+
+
+parser = argparse.ArgumentParser(description='LFR coverage report generator')
+parser.add_argument('--src-dir')
+parser.add_argument('--build-dir')
+parser.add_argument('--gcov-output-dir')
+parser.add_argument('--gcov-path')
+parser.add_argument('--output-dir')
+args = parser.parse_args()
+
+
+build_gcov_files = f"{os.path.dirname(__file__)}/build_gcov_files.py"
+
+gcov_traces = list(filter(lambda f:f.endswith(".txt") ,os.listdir(args.gcov_output_dir)))
+
+def generate_individual_report(gcov_trace):
+    output_dir = f"{args.output_dir}/{gcov_trace.replace('.txt','')}"
+    os.makedirs(output_dir, exist_ok=True)
+    subprocess.run([build_gcov_files, "-r", args.build_dir, "-o", output_dir, f"{args.gcov_output_dir}/{gcov_trace}"])
+    subprocess.run(["lcov", "--gcov-tool", args.gcov_path, '-c', '-d', output_dir, '-b', args.build_dir ,  '-o', f'{output_dir}/trace.info'])
+    subprocess.run(["genhtml", '-o', f'{output_dir}',  f'{output_dir}/trace.info'])
+    return f'{output_dir}/trace.info'
+
+def generate_total_coverage_report(lcov_traces):
+    output_dir = f"{args.output_dir}/total_coverage"
+    os.makedirs(output_dir, exist_ok=True)
+    lcov_args = ["lcov" ]
+    for t in lcov_traces:
+        lcov_args+= ['-a',t]
+    lcov_args += ["-o", f"{output_dir}/trace.info"]
+    subprocess.run(lcov_args)
+    subprocess.run(["genhtml", '-o', f'{output_dir}', f"{output_dir}/trace.info"])
+
+generate_total_coverage_report(list(map(generate_individual_report, gcov_traces)))
